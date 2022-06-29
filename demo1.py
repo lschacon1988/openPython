0b041fb1377795f5695c7d795386e41da7765688
lschacon1988 <lschacon1988@gmail.com>
2022-06-29 11:18:53 -0500

    Initial commit

---
+++ demo1.py
@@ -0,0 +1,9 @@
+nombre = 'luis'
+print('Hola, soy'+' '+nombre)
+
+print('Estoy empezando el curso de python.')
+
+print('Espero aprender mucho.')
+
+nombre='Elian'
+print('Ahora soy'+' '+nombre+'.')
