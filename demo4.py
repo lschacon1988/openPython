01530ec2f42e8a3d9826bd3106eae979199cae77
lschacon1988 <lschacon1988@gmail.com>
2022-06-30 09:49:44 -0500

    completo ejercico 4

--- demo4.py
+++ demo4.py
@@ -1,5 +1,17 @@
 import math
 
+nombre='Luis'
+apellido='Chacon'
+edad=34
+email='lschacon26@gmail.com'
+casado=True
+conHijos=True
+amigos=['Julmary', 'Jhon']
+peliculasVistas={
+    'titulo': 'Sherk',
+    'titulo':'iron man'
+}
+
 
 peso = float(input("Ingrese su peso en Kilogramos: "))
 estatura = float(input("Ingrese su estatura en metros: "))
