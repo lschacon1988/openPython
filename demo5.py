357847cb54ac9f099a2477ca4eb1982a7c99eebc
lschacon1988 <lschacon1988@gmail.com>
2022-07-12 15:46:36 -0500

    verificacion de edad

---
+++ demo5.py
@@ -0,0 +1,8 @@
+
+edad = int(input("Que edad tienes: "))
+
+
+if edad > 18:
+    print("El usuario es mayor de edad tiene " + str(edad) + " años")
+else:
+    print("El usuario no es mayor de edad tiene " + str(edad) + " años")
